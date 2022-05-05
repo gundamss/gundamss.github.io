f76f74b5da478cf54862f9d331348d1485564999
gundamss <chauyuhin1998@gmail.com>
2022-05-05 15:01:30 -0600

    lol

---
+++ solo_prep_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace solo_prep_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            do{
+                Console.Write("Do you want to continue?");
+                input = Console.ReadLine();
+
+            }while (input =="yes");
+            string i = input;
+            for (int i = 2; i<=20; i=i+2)
+                {
+                    Console.WriteLine(i);
+                }
+            Console.Write("What is your last name?");
+            string last = Console.ReadLine();           
+            Console.WriteLine($"Your name is ,{last}");
+        }
+    }
+}
---
+++ solo_prep_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace solo_prep_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+            int rannum = randomGenerator.Next(1,9);
+            int start_num = -1;
+            foreach (string word in words)
+            while (start_num != rannum)
+            {
+                Console.Write("What is your guess? ");
+                guess_num = int.Parse(Console.ReadLine());
+
+                if (rannum > guess_num)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (rannum < guess_num)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+        }
+    }
+}
